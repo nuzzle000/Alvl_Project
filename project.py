1080f4aefa31171475ca1b73894c62812783defc
nuzzle000 <jop.10@hotmail.co.uk>
2021-01-07 11:41:51 +0000

    Basic button

--- project.py
+++ project.py
@@ -1,5 +1,9 @@
 import tkinter
+from tkinter import tk
 mainWin = tkinter.Tk()
 mainWin.title("Untitled")
+mainWin.geometry("600x600")
+
+btn1 = tkinter.Button(mainWin, text="Hello").pack()
 
 mainWin.mainloop()
