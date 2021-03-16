88930ecd166648bbba2c88cd49440c5d17ec200e
nuzzle000 <35398212+nuzzle000@users.noreply.github.com>
2021-03-16 10:58:27 +0000

    GUI Input Done

--- main.py
+++ main.py
@@ -224,8 +224,29 @@ ttk.Label(TAB5, text='Details:').grid(column=0,row=1,padx=10,pady=10)
 LearnerDet = StringVar()
 
 ttk.Entry(TAB5, textvariable=LearnerID).grid(column=1,row=0,padx=10,pady=10)
-Text(TAB5).grid(column=1,row=1,padx=50,pady=10)
+Text(TAB5, width=50, height=20, wrap=WORD).grid(column=1,row=1,padx=10,pady=10)
 
+#TAB6 Contents - Add Plan Details
+ttk.Label(TAB6, text='Select Lesson:').grid(column=0,row=0,padx=10,pady=10)
+ttk.Label(TAB6, text='Enter Plan:').grid(column=0,row=1,padx=10,pady=10)
+
+ttk.Combobox(TAB6).grid(column=1,row=0,padx=10,pady=10)
+Text(TAB6, width=50, height=20, wrap=WORD).grid(column=1,row=1,padx=10,pady=10)
+
+#TAB7 Contents - Add Lesson Details
+ttk.Label(TAB7, text='Add Location:').grid(column=0,row=0,padx=10,pady=10)
+ttk.Label(TAB7, text='Add Class ID:').grid(column=0,row=1,padx=10,pady=10)
+ttk.Label(TAB7, text='Add Subject ID:').grid(column=0,row=2,padx=10,pady=10)
+ttk.Label(TAB7, text='Add Teacher ID:').grid(column=0,row=3,padx=10,pady=10)
+ttk.Label(TAB7, text='Add Period ID:').grid(column=0,row=4,padx=10,pady=10)
+ttk.Label(TAB7, text='Add Plan ID:').grid(column=0,row=5,padx=10,pady=10)
+
+ttk.Entry(TAB7, textvariable=).grid(column=1,row=0,padx=10,pady=10)
+ttk.Entry(TAB7, textvaraible=).grid(column=1,row=1,padx=10,pady=10)
+ttk.Entry(TAB7, textvaraible=).grid(column=1,row=2,padx=10,pady=10)
+ttk.Entry(TAB7, textvaraible=).grid(column=1,row=3,padx=10,pady=10)
+ttk.Entry(TAB7, textvaraible=).grid(column=1,row=4,padx=10,pady=10)
+ttk.Entry(TAB7, textvaraible=).grid(column=1,row=5,padx=10,pady=10)
 
 mainWin.mainloop()
 
